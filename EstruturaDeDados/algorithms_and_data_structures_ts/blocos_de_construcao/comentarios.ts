71df7d7a68f1c8d59589110ec1e37b2402b2ac0e
Mauro <maurodopradosantos@gmail.com>
2024-03-06 22:32:39 -0300

    updt

--- EstruturaDeDados/algorithms_and_data_structures_ts/blocos_de_construcao/comentarios.ts
+++
@@ -1,8 +0,0 @@
-/* Exemplo de comentário em várias linhas
-**
-** Autor: Fabrício G. M. de Carvalho
-*/
-
-var a: number = 10; // um número qualquer.
-console.log(a);
-
--- EstruturaDeDados/algorithms_and_data_structures_ts/recursao/maior_recursivo_view.ts
+++
@@ -1,8 +0,0 @@
-import { maior_r } from "./maior_recursivo_model";
-
-let a: number[] = [1, 4, 10, 20, -1];
-console.log("Array original:");
-console.log(a);
-let maior = maior_r(a);
-console.log("O maior número do array é: ", maior);
-
